0b459e2331666fb83be786214b6532d7b007ae75
diogo-sant0s <githubpersonal0@gmail.com>
2026-02-18 17:04:19 +0000

    Add files via upload

--- Site Desprotegido/views.py
+++ Site Desprotegido/views.py
@@ -4,32 +4,28 @@ from main import app
 from sqlalchemy import text
 
 @app.route("/")
-def home():
-    return redirect(url_for("login"))
-
+def index():
+    return render_template("login.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "GET":
-        return render_template("login.html")
+    
+        login_input = request.form.get("username")
+        password_input = request.form.get("password")
 
-    login_input = request.form.get("username")
-    password_input = request.form.get("password")
-
-    query = f"SELECT * FROM id WHERE username = '{login_input}' AND password = '{password_input}'"
-    result = db_session.execute(text(query)).fetchnone()
+    query = f"SELECT * FROM login WHERE username = '{login_input}' AND password = '{password_input}'"
+    result = db_session.execute(text(query)).fetchone()
 
     if result:
-        flask_session['user'] = login_input
-        return redirect(url_for('dashboard'))
+        flask_session["user_id"] = login_input
+        return redirect(url_for('main.dashboard'))
     
     else:
-        return redirect(url_for("login.html"))
+        return redirect(url_for("login"))
 
 
 
 @app.route("/dashboard")
 def dashboard():
-    if "user_id" not in flask_session:
-        return redirect(url_for("login"))
     return render_template("dashboard.html")
